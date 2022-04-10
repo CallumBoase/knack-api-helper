6757a2121d6809e21b29f2c92aa6fd576953728b
CallumBoase <callum.boase@gmail.com>
2022-04-10 20:33:15 +1000

    fix url construction view based get

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ const knackAPI = {
     },
     viewBased: {
         async get(settings){
-            const url = `https://api.knack.com/v1/pages/${scene}/views/${view}/records`;
+            const url = `https://api.knack.com/v1/pages/${settings.scene}/views/${settings.view}/records`;
             if(filters) url += `?${this.buildFilters(settings.filters)}`;
             const options = {
                 method: 'GET',
