6e97b8829a1b6e76adb7e019d82a67f2dc60c9e2
CallumBoase <callum.boase@gmail.com>
2022-06-03 17:09:31 +1000

    make dynamically nodejs

--- KnackAPI.js
+++ KnackAPI.js
@@ -1,4 +1,6 @@
-const fetch = require('node-fetch');
+if(require){
+    const fetch = require('node-fetch');
+}
 
 function delay(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -364,7 +366,7 @@ function KnackAPI(config) {
     }
 }
 
-if(typeof module != "undefined" && typeof module.exports != "undefined"){
+if(module && module.exports){
     //https://medium.com/@gaute.meek/how-to-publish-a-js-library-to-npm-and-cdn-9d0bf9b48e11
    module.exports = KnackAPI;
 }
